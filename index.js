624f7def4fbdc0e0e6407c0d23c9b649d196d8a7
Amoolya Jain <amoolyajain2002@gmail.com>
2022-09-03 18:50:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function change(){
+document.getElementById("test").innerHTML="Hello World";
+
+document.getElementById("test").style.fontSize="50px";
+}
+function change1(){
+  document.getElementById("test").style.color="blue";
+}
